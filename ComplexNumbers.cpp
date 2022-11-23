b1d031f5a72b2f499cf19eb8e562d435c0bfcedc
Юрий Наумов <ynaumov@digitrans.ru>
2022-11-23 11:27:30 +0300

    Первая версия. Добавлена структура комплексных чисел

---
+++ ComplexNumbers.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct ComplexNumber
+{
+    double Re;
+    double Im;
+};
+
+int main()
+{
+    //Включаем локализацию
+    system("chcp 1251");
+    //Создаем одно комплексное число
+    ComplexNumber z = { 1,1 };
+    //Вывод числа в консоль
+    cout << "Комплексное число z = " << z.Re << " + " << z.Im << "i" << endl;
+   
+}
+
